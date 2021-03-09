3ab53d16b278dcf2389c22bf4bc7a812346c43d1
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-09 14:49:14 +0700

    Add Torus integration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Torus from "@toruslabs/torus-embed";
+import Web3 from "web3";
+import wordmark from "./wordmark.svg";
+import "./App.css";
 
 function App() {
+  const [account, setAccount] = useState();
+
+  const onClickLogin = async (e) => {
+    e.preventDefault();
+
+    const torus = new Torus({});
+    await torus.init({
+      enableLogging: false,
+    });
+    await torus.login();
+
+    const web3 = new Web3(torus.provider);
+    const address = (await web3.eth.getAccounts())[0];
+    const balance = await web3.eth.getBalance(address);
+    setAccount({ address, balance });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={wordmark} className="App-logo" alt="logo" />
+        {account ? (
+          <div className="App-info">
+            <p><strong>Address</strong>: {account.address}</p>
+            <p><strong>Balance</strong>: {account.balance}</p>
+          </div>
+        ) : (
+          <>
+            <p>You didn't login yet. Login to see your account details.</p>
+            <button className="App-link" onClick={onClickLogin}>
+              Login
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
